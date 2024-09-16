b0c829ec0b29a472f2a91c29bedb4f2233dcb4e0
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-16 14:41:23 +0545

    add appealing color

--- src/components/About/Card.jsx
+++ src/components/About/Card.jsx
@@ -11,7 +11,7 @@ function Card({
   return (
     <div class="relative w-96 h-96 perspective-1000">
       <div 
-      className='absolute w-64 h-72 bg-[#FEEACD] shadow-sm  shadow-black transform rotate-x-12 rotate-y-6 z-10 rounded-xl '>
+      className='absolute w-64 h-72 bg-gradient-to-t from-red-300 to-white shadow-sm  shadow-black transform rotate-x-12 rotate-y-6 z-10 rounded-xl '>
         <h1 className='text-center font-playball font-medium text-xl mt-4 mb-1 text-[#3E3A3A]'> {title}</h1>
         <p className='mx-5 font-serif text-[#504F4F] text-[0.62rem] font-medium'>{desc}</p>
         <img 
@@ -20,7 +20,7 @@ function Card({
         className='w-20 mt-9 mx-2 lg:w-20 rounded-full'/>
       </div>
 
-      <div className='absolute w-44 h-44 sm:w-52 sm:h-52 bg-[#FEEACD] shadow-sm shadow-black transform translate-x-[60%] translate-y-[80%] sm:translate-x-1/2 sm:translate-y-[65%] lg:translate-x-[44%] z-20 rounded-full'>
+      <div className='absolute w-44 h-44 sm:w-52 sm:h-52 bg-gradient-to-b from-red-300 to-white shadow-sm shadow-black transform translate-x-[60%] translate-y-[80%] sm:translate-x-1/2 sm:translate-y-[65%] lg:translate-x-[44%] z-20 rounded-full'>
         <img src={url} alt=""
         className='rounded-full p-1' />
       </div>
--- src/components/About/WhyLipLuxe.jsx
+++ src/components/About/WhyLipLuxe.jsx
@@ -4,13 +4,13 @@ import Card from './Card'
 function WhyLipLuxe() {
   return (
     <>
-      <div className="px-6 sm:px-6 lg:px-8">
+      <div className="px-6 sm:px-6 lg:px-8 bg-[#f7f7f7]">
         <h1 className="text-center  text-2xl font-extrabold font-cinzel tracking-wider text-gray-800 my-10 underline underline-offset-1">
           Why LipLuxe ?
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-7 sm:space-x-9 sm:pl-6 lg:mr-11 xl:mr-0 xl:space-x-0 xl:ml-40 mL:pl-24 mM:pl-16">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-7 sm:space-x-9 sm:pl-6 lg:mr-11 xl:mr-0 xl:space-x-0 xl:ml-40 mL:pl-24 mM:pl-16 bg-[#f7f7f7]">
         <div className='sm:pl-10 lg:pl-8 xl:pl-0'>
           <Card 
           title="Premium Quality Ingredients"
