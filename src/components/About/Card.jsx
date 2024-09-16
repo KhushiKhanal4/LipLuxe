b8713f63bdb4c98d9de4c1a2fca21a61cbc1b12f
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-16 14:18:14 +0545

    add card dynamic elements

--- src/components/About/Card.jsx
+++ src/components/About/Card.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 
-function Card() {
+
+function Card({
+  title,
+  desc,
+  url,
+  png,
+}) {
+  
   return (
     <div class="relative w-96 h-96 perspective-1000">
       <div 
       className='absolute w-64 h-72 bg-[#FEEACD] shadow-sm  shadow-black transform rotate-x-12 rotate-y-6 z-10 rounded-xl '>
-        <h1 className='text-center font-playball font-medium text-xl mt-4 mb-1 text-[#3E3A3A]'> Premium Quality Ingredients</h1>
-        <p className='mx-5 font-serif text-[#504F4F] text-[0.62rem] font-medium'>At Lip Luxe, we use only the finest, skin-friendly ingredients to ensure our lipsticks not only look stunning but also nourish your lips. Our formulas are enriched with natural oils and butters, providing a smooth application while being cruelty-free and free from harmful chemicals</p>
+        <h1 className='text-center font-playball font-medium text-xl mt-4 mb-1 text-[#3E3A3A]'> {title}</h1>
+        <p className='mx-5 font-serif text-[#504F4F] text-[0.62rem] font-medium'>{desc}</p>
         <img 
-        src="About\avocado-removebg-preview.png" 
+        src={png}
         alt="" 
-        className='w-24 mt-9 mx-2 lg:w-20'/>
+        className='w-20 mt-9 mx-2 lg:w-20 rounded-full'/>
       </div>
 
       <div className='absolute w-44 h-44 sm:w-52 sm:h-52 bg-[#FEEACD] shadow-sm shadow-black transform translate-x-[60%] translate-y-[80%] sm:translate-x-1/2 sm:translate-y-[65%] lg:translate-x-[44%] z-20 rounded-full'>
-        <img src="About\ingredient.jpg" alt=""
+        <img src={url} alt=""
         className='rounded-full p-1' />
       </div>
     
--- src/components/About/WhyLipLuxe.jsx
+++ src/components/About/WhyLipLuxe.jsx
@@ -12,13 +12,37 @@ function WhyLipLuxe() {
 
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-7 sm:space-x-9 sm:pl-6 lg:mr-11 xl:mr-0 xl:space-x-0 xl:ml-40 mL:pl-24 mM:pl-16">
         <div className='sm:pl-10 lg:pl-8 xl:pl-0'>
-        <Card />
+          <Card 
+          title="Premium Quality Ingredients"
+          desc="At Lip Luxe, we use only the finest, skin-friendly ingredients to ensure our lipsticks not only look stunning but also nourish your lips. Our formulas are enriched with natural oils and butters, providing a smooth application while being cruelty-free and free from harmful chemicals."
+          url="public\About\ingredient.jpg"
+          png="public\About\avocado-removebg-preview.png"/>
         </div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card 
+          title="Long-lasting Formula"
+          desc="Our lipsticks are crafted for long-lasting wear, offering a rich color payoff that stays vibrant and true throughout the day. Whether you're attending a meeting or enjoying a night out, Lip Luxe ensures your look remains flawless and fresh."
+          url="public\About\waterproof.jpg"
+          png="public\About\timer.png"/>
+        <Card 
+          title="Vibrant & Diverse Shades"
+          desc="Discover a spectrum of shades that cater to every skin tone and occasion. From bold reds to subtle nudes, our collection ensures you find the perfect hue that enhances your natural beauty and complements any look."
+          url="public\About\colors.jpg"
+          png="public\About\shades.png"/>
+        <Card 
+          title="Hydration & Comfort"
+          desc="Lip Luxe lipsticks are designed to keep your lips hydrated and comfortable all day long. Our blend of moisturizing ingredients prevents dryness, providing a creamy texture that feels light and luxurious on your lips."
+          url="public\About\soft1.jpg"
+          png="public\About\drops.png"/>
+        <Card 
+          title="Luxury Packaging"
+          desc="Experience elegance with every application. Our lipsticks come in beautifully designed packaging that reflects our commitment to luxury and sophistication. The sleek, stylish cases are perfect for on-the-go glamour."
+          url="public\About\pack.png"
+          png="public\About\slim.png"/>
+        <Card 
+          title="Eco-friendly Initiatives"
+          desc="At Lip Luxe, we are committed to sustainability. Our packaging is made from recyclable materials, and we are constantly working to reduce our environmental impact. Enjoy your beauty routine knowing you’re making a responsible choice."
+          url="public\About\green.jpg"
+          png="public\About\images-Photoroom.png"/>
       </div>
     </>
   )
