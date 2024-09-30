cf59efe674961e745322b33f287c6c3ae3c1beb7
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-30 19:44:53 +0545

    create lipstick data

---
+++ src/data/lipstick.js
@@ -0,0 +1,51 @@
+const lipstick = [
+    {
+        id: 1,
+        Name: "Classic Crimson Satin Lipstick",
+        price: " $6.99 USD",
+        image: "./lipstick/lipstick1.png",
+    },
+    {
+        id: 2,
+        Name: "Rose Gold Radiance Pink Lipstick",
+        price: " $15.00 USD",
+        image: "./lipstick/lipstick2.png",
+    },
+    {
+        id: 3,
+        Name: "Crimson Charm Liquid Lipstick",
+        price: " $25.00 USD",
+        image: "./lipstick/lipstick3.png",
+    },
+    {
+        id: 4,
+        Name: "Dusky Pink Charm Lipstick",
+        price: " $10.00 USD",
+        image: "./lipstick/lipstick4.png",
+    },
+    {
+        id: 5,
+        Name: "Classy Brick Red Lipstick",
+        price: " $12.00 USD",
+        image: "./lipstick/lipstick5.png",
+    },
+    {
+        id: 6,
+        Name: "Classic Rouge",
+        price: " $7.00 USD",
+        image: "./lipstick/lipstick6.png",
+    },
+    {
+        id: 7,
+        Name: "Matte Charm Pink Lipstick",
+        price: " $28.99 USD",
+        image: "./lipstick/lipstick7.png",
+    },
+    {
+        id: 8,
+        Name: "Radiant Rouge Metallic Lipstick",
+        price: " $30.00 USD",
+        image: "./lipstick/lipstick8.png",
+    },
+
+]
