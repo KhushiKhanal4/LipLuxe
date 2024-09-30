1c82cab7085bd1d6c252a6d13add965b175d0add
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-30 17:31:41 +0545

    create lipcare data

---
+++ src/data/lipcare.js
@@ -0,0 +1,39 @@
+const lipcare = [
+    {
+        id: 1,
+        Name: "Chic Shine Lip Glow Oil",
+        price: " $20.00 USD",
+        image: "./Lipcare/lip oil.jpg",
+    },
+    {
+        id: 2,
+        Name: "Kissable Cushion Lip Mask",
+        price: " $25.00 USD",
+        image: "./Lipcare/lipmask.jpg",
+    },
+    {
+        id: 3,
+        Name: "Sheer Elegance Lipstick",
+        price: " $25.00 USD",
+        image: "./Lipcare/lipmask2.jpg",
+    },
+    {
+        id: 4,
+        Name: "Juicy Melon Lip Scrub",
+        price: " $15.00 USD",
+        image: "./Lipcare/scrub1-Photoroom.jpg",
+    },
+    {
+        id: 5,
+        Name: "Juicy Berry Lip Scrub",
+        price: " $15.00 USD",
+        image: "./Lipcare/scrub2-Photoroom.jpg",
+    },
+    {
+        id: 6,
+        Name: "Soft Tint Lip Mask",
+        price: " $20.00 USD",
+        image: "./Lipcare/tint balm.jpg",
+    }
+
+]
