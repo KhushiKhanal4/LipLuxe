10971d962449977e7630192ace2144857de329fe
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-30 18:45:45 +0545

    edit hamburger menu

--- src/components/Header/BurgerNavItem.jsx
+++ src/components/Header/BurgerNavItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BurgerNavItem(props) {
   return (
     <>
-    <a href="#" className="text-gray-900 hover:bg-[#ffffff4d] hover:text-red-700 hover:border hover:border-red-50 block px-3 py-2 rounded-md text-base font-bold transition duration-300 tracking-widest text-shadow-sm">{props.title}</a>
+    <div className="text-gray-900 hover:bg-[#ffffff4d] hover:text-red-700 hover:border hover:border-red-50 block px-3 py-2 rounded-md text-base font-bold transition duration-300 tracking-widest text-shadow-sm">{props.title}</div>
     </>
   )
 }
