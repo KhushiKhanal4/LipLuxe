536999b2632bc5b470758e044f7db1e0622b2822
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-30 18:44:13 +0545

    create lipgloss data

---
+++ src/data/lipgloss.js
@@ -0,0 +1,51 @@
+const lipgloss = [
+    {
+        id: 1,
+        Name: "Juicy Tubes lip gloss",
+        price: " $20.00 USD",
+        image: "./lipGloss/gloss.png",
+    },
+    {
+        id: 2,
+        Name: "Peach Kisses Lip Gloss",
+        price: " $15.00 USD",
+        image: "./lipGloss/gloss2.png",
+    },
+    {
+        id: 3,
+        Name: "Glossier Pink Shimmer Lip Gloss",
+        price: " $18.00 USD",
+        image: "./lipGloss/gloss3.png",
+    },
+    {
+        id: 4,
+        Name: "Transparent Shine Lip Gloss",
+        price: " $10.00 USD",
+        image: "./lipGloss/gloss4.png",
+    },
+    {
+        id: 5,
+        Name: "Crystal Shine Lip Gloss",
+        price: " $12.00 USD",
+        image: "./lipGloss/gloss5.png",
+    },
+    {
+        id: 6,
+        Name: "Shimmer Tint Lip Gloss",
+        price: " $28.00 USD",
+        image: "./lipGloss/gloss6.png",
+    },
+    {
+        id: 7,
+        Name: "Clear Gleam Lip Gloss",
+        price: " $25.00 USD",
+        image: "./lipGloss/glossy finish-Photoroom.jpg",
+    },
+    {
+        id: 8,
+        Name: "Plump & Shine Lip Volumizer",
+        price: " $22.00 USD",
+        image: "./lipGloss/lip volumizer.jpg",
+    },
+
+]
