f9e675213805365c6d83d6c84c17f3cf28573e38
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-10-05 19:23:57 +0545

    create items added modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,7 +8,7 @@ function Modal({
 }) {
   return (
     <div
-    className={`flex justify-center items-center fixed inset-0 transition-colors ${ open ? "visible bg-black/35":"invisible"}`}>
+    className={`flex justify-center items-center fixed inset-0 transition-colors ${ open ? "visible bg-black/20":"invisible"}`}>
         <div className={`bg-gray-50 mx-3 max-w-md  rounded-lg shadow-lg shadow-gray-500 transition-all duration-300 ${open?"scale-100 opacity-100":"scale-125 opacity-0"}`}>
             <button className='float-right px-2 my-1.5'
             onClick={close}><RxCross2 className='text-xl text-gray-700'/></button>
